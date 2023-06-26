b24f51594666bfb1c0e5867cd5f41e52ffe157f1
Gabriel Martinez <gabriel.emv411@gmail.com>
2023-06-25 23:59:16 -0600

    Reverted prior changes, moved passwordencoder

--- src/main/java/com/bancrabs/villaticket/VillaticketApplication.java
+++ src/main/java/com/bancrabs/villaticket/VillaticketApplication.java
@@ -1,22 +1,12 @@
 package com.bancrabs.villaticket;
 
-import java.security.SecureRandom;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class VillaticketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VillaticketApplication.class, args);
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder(5, new SecureRandom());
 	}
 }
