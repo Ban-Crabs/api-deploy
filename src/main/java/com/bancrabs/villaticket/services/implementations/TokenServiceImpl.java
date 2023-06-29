dbae3f81ef39e917ec0e00d5009840631ee6b90f
Gabriel Martinez <gabriel.emv411@gmail.com>
2023-06-28 20:57:06 -0600

    Added Service annotation

--- src/main/java/com/bancrabs/villaticket/services/implementations/TokenServiceImpl.java
+++ src/main/java/com/bancrabs/villaticket/services/implementations/TokenServiceImpl.java
@@ -3,11 +3,13 @@ package com.bancrabs.villaticket.services.implementations;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.bancrabs.villaticket.models.entities.Token;
 import com.bancrabs.villaticket.repositories.TokenRepository;
 import com.bancrabs.villaticket.services.TokenService;
 
+@Service
 public class TokenServiceImpl implements TokenService{
 
     @Autowired
